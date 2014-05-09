a767106048254f96c0a55c7e8ff88e14c3566265
maxime.jumel <maxime.jumel@PC030102324.HEADQUARTER.hq.geodis.org>
2014-05-09 17:38:33 +0200

    v0.0.14.1

--- MyStories/src/com/mjumel/mystories/adapters/EventListAdapter.java
+++ MyStories/src/com/mjumel/mystories/adapters/EventListAdapter.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 
 import com.mjumel.mystories.Event;
@@ -64,51 +66,43 @@ public class EventListAdapter extends ArrayAdapter<Event> {
 		
 		if (event.getRating() < 0)
 			holder.rating.setVisibility(RatingBar.INVISIBLE);
-		else
-		{
+		else {
 			holder.rating.setVisibility(RatingBar.VISIBLE);
 			holder.rating.setProgress(event.getRating());
 		}
 		
-		if (event.getThumbMediaPath() != null)
-		{
+		if (event.getThumbMediaPath() != null) {
 			Gen.appendLog("EventListAdapter::getView> Image loading for event#" + position + " (" + event.getThumbMediaPath() + ")");
 			holder.image.setVisibility(ImageView.VISIBLE);
-			
 			ImageLoader.getInstance().displayImage(event.getThumbMediaPath(), holder.image);
-			
-			//imageLoader.DisplayImage(event.getThumbMediaPath(), holder.image);
-			
-			// Change layout display in function of the position of the view in the list
-			/*LayoutParams lpImage = new LayoutParams(80,80);
-			LayoutParams lpText = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-			if (position%2 == 0) {
-				lpImage.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, -1);
-				lpText.addRule(RelativeLayout.LEFT_OF, R.id.event_item_imageView);
-			} else {
-				lpImage.addRule(RelativeLayout.ALIGN_PARENT_LEFT, -1);
-				lpText.addRule(RelativeLayout.RIGHT_OF, R.id.event_item_imageView);
-			}
-			holder.image.setLayoutParams(lpImage);
-			holder.comment.setLayoutParams(lpText);
-			//holder.rating.setLayoutParams(lpText);
-			 */
-			
-			/*UserPicture userPicture = new UserPicture(Uri.parse(event.getThumbMediaPath()), context.getContentResolver());
-	        try {
-	        	holder.image.setImageBitmap(userPicture.getBitmap());
-			} catch (IOException e) {
-				e.printStackTrace();
-				Gen.appendLog("EventListAdapter::getView> IOException Error ("+event.getThumbMediaPath()+")");
-			}
-	        userPicture = null;*/
-		}
-		else
-		{
+		} else {
 			holder.image.setImageBitmap(null);
 			holder.image.setVisibility(ImageView.INVISIBLE);
 		}
-
+			
+		// Change layout display in function of the position of the view in the list
+		/*LayoutParams lpRow = new LayoutParams(LayoutParams.MATCH_PARENT, 80);
+		LayoutParams lpImage = new LayoutParams(80,LayoutParams.MATCH_PARENT);
+		LayoutParams lpText = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+		LayoutParams lpRating = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+		if (position%2 == 0) {
+			lpImage.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, -1);
+			lpText.addRule(RelativeLayout.ALIGN_PARENT_TOP, -1);
+			lpText.addRule(RelativeLayout.LEFT_OF, R.id.event_item_imageView);
+			lpRating.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, -1);
+			lpRating.addRule(RelativeLayout.ALIGN_PARENT_LEFT, -1);
+		} else {
+			lpImage.addRule(RelativeLayout.ALIGN_PARENT_LEFT, -1);
+			lpText.addRule(RelativeLayout.ALIGN_PARENT_TOP, -1);
+			lpText.addRule(RelativeLayout.RIGHT_OF, R.id.event_item_imageView);
+			lpRating.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, -1);
+			lpRating.addRule(RelativeLayout.RIGHT_OF, R.id.event_item_imageView);
+		}
+		//rowView.setLayoutParams(lpRow);
+		holder.image.setLayoutParams(lpImage);
+		holder.comment.setLayoutParams(lpText);
+		holder.rating.setLayoutParams(lpRating);*/
+		
         //Gen.appendLog("EventListAdapter::getView> Ending event#"+position);
 		return rowView;
 	}
---
+++ MyStories/src/com/mjumel/mystories/adapters/StoryListAdapter.java
@@ -0,0 +1,67 @@
+package com.mjumel.mystories.adapters;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.mjumel.mystories.R;
+import com.mjumel.mystories.Story;
+
+public class StoryListAdapter extends ArrayAdapter<Story> {
+	private LayoutInflater inflater;
+	
+	static class ViewHolder {
+		TextView title;
+		TextView desc;
+		TextView viewers;
+		RatingBar rating;
+	}
+
+	public StoryListAdapter(Activity context, List<Story> stories) {
+		super(context, R.layout.fragment_my_stories_item, stories);
+		this.inflater = LayoutInflater.from(context);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View rowView = convertView;
+		ViewHolder holder;
+		
+		//Gen.appendLog("StoryListAdapter::getView> Starting event#"+position);
+		
+		if ( rowView == null ) {
+			rowView = inflater.inflate(R.layout.fragment_my_stories_item, null);
+			holder = new ViewHolder();
+            holder.title = (TextView) rowView.findViewById(R.id.my_stories_item_title);
+            holder.desc = (TextView) rowView.findViewById(R.id.my_stories_item_desc);
+            holder.viewers = (TextView) rowView.findViewById(R.id.my_stories_item_viewers);
+            holder.rating = (RatingBar) rowView.findViewById(R.id.my_stories_item_ratingBar);
+            rowView.setTag(holder);
+        } else {
+            holder = (ViewHolder) rowView.getTag();
+        }
+		
+		Story story = this.getItem(position);
+		
+		holder.title.setText(story.getTitle());
+		holder.desc.setText(story.getEventsCount() + " events, created on xx/xx/xxxx");
+		holder.viewers.setText(String.valueOf(Math.round(Math.random()*10)) + " views");
+		
+		/*if (story.getRating() < 0)
+			holder.rating.setVisibility(RatingBar.INVISIBLE);
+		else
+		{*/
+			holder.rating.setVisibility(RatingBar.VISIBLE);
+			holder.rating.setProgress(story.getRating());
+		//}
+
+        //Gen.appendLog("EventListAdapter::getView> Ending event#"+position);
+		return rowView;
+	}
+}
